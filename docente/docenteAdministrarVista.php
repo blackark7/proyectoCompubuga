ef149f2a968cee2aa50b936ef6e674c44b97fe65
LOPEZ <LOPEZ@DESKTOP-6SKP540>
2019-10-18 19:51:29 -0500

    se puso un hola

--- docente/docenteAdministrarVista.php
+++ docente/docenteAdministrarVista.php
@@ -41,6 +41,7 @@
         ?>
         <div class="container">
          <h1>DOCENTES</h1>
+         holallalalala
          <div class="card shadow mb-4">
           <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">Lista de Docentes</h6>
