422f7c84e9743c8645f39afbf939bda274e5fec3
LOPEZ <LOPEZ@DESKTOP-6SKP540>
2019-10-21 14:37:25 -0500

    se hizo el carreraModelo

--- carrera/carrerasModelo.php
+++ carrera/carrerasModelo.php
@@ -1,13 +1,20 @@
 <?php
-    include_once '../conexion.php';
-    class carrerasModelo
-    {
-        function mostrarTodos(){
-            global $link;
-            $consulta = "SELECT * FROM `carrera`";
-            $query = mysqli_query($link, $consulta);
-            return $query;
-        }
 
+include_once '../conexion.php';
 
+class carrerasModelo {
+
+    function insertar($nombre) {
+        global $link;
+        $consulta = "INSERT INTO `carrera` (`id`, `nombre`) VALUES (NULL, '$nombre');";
+        $query = mysqli_query($link, $consulta);
     }
+
+    function mostrarTodos() {
+        global $link;
+        $consulta = "SELECT * FROM `carrera`";
+        $query = mysqli_query($link, $consulta);
+        return $query;
+    }
+
+}
