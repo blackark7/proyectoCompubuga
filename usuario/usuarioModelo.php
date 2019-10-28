7e05c44ee9cc661595b74afd2547a446d9f789a8
LOPEZ <LOPEZ@DESKTOP-KPU8R5E>
2019-10-28 17:12:31 -0500

    se modifica de todo un poquito

--- usuario/usuarioModelo.php
+++ usuario/usuarioModelo.php
@@ -1,15 +1,21 @@
 <?php
-    include_once "../conexion.php";
 
-    class usuarioModelo
-    {
-        function insertar($tipo, $usuario, $password)
-        {
-            $pass = md5($password);
-            global $link;
-            $consulta = "INSERT INTO `usuarios` (`id`, `user`, `password`, `tipo`) VALUES (NULL, '$usuario', '$pass', '$tipo');";
-            $query = mysqli_query($link, $consulta);
+include_once "../conexion.php";
 
-        }
+class usuarioModelo {
 
+    function insertar($tipo, $usuario, $password) {
+        $pass = md5($password);
+        global $link;
+        $consulta = "INSERT INTO `usuarios` (`id`, `user`, `password`, `tipo`) VALUES (NULL, '$usuario', '$pass', '$tipo');";
+        $query = mysqli_query($link, $consulta);
     }
+
+    function mostrarTodos() {
+        global $link;
+        $consulta = "SELECT * FROM `usuarios`";
+        $query = mysqli_query($link, $consulta);
+        return $query;
+    }
+
+}
