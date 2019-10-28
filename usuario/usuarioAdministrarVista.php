e711eef804008bcc1803a98675f29e86d71bff72
LOPEZ <LOPEZ@DESKTOP-KPU8R5E>
2019-10-28 17:12:08 -0500

    se modifica de todo un poquito

--- usuario/usuarioAdministrarVista.php
+++ usuario/usuarioAdministrarVista.php
@@ -1,80 +1,85 @@
+<?php
+include_once "../componets/enrutamientoSeguridad.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+    <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-  <title>Compubuga Admin</title>
+        <title>Compubuga Admin</title>
 
-  <!-- Custom fonts for this template-->
-  <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+        <!-- Custom fonts for this template-->
+        <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-  <!-- Custom styles for this template-->
-  <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+        <!-- Custom styles for this template-->
+        <link href="../css/sb-admin-2.min.css" rel="stylesheet">
 
-</head>
+    </head>
 
-<body id="page-top">
+    <body id="page-top">
 
-  <!-- Page Wrapper -->
-  <div id="wrapper">
+        <!-- Page Wrapper -->
+        <div id="wrapper">
 
-    <!-- Sidebar menu despegable   -->
-    <?php 
+            <!-- Sidebar menu despegable   -->
+            <?php
+            include_once '../componets/menu.php';
+            ?>
+            <!-- Content Wrapper -->
+            <div id="content-wrapper" class="d-flex flex-column">
 
-    include_once '../componets/menu.php';
-    ?>
-    <!-- Content Wrapper -->
-    <div id="content-wrapper" class="d-flex flex-column">
+                <!-- Main Content -->
+                <div id="content">
+                    <?php
+                    include_once '../componets/navBar.php';
+                    ?>
+                    <div class="container">
+                        <h1>USUARIOS</h1>
+                        <div class="card shadow mb-4">
+                            <div class="card-header py-3">
+                                <h6 class="m-0 font-weight-bold text-primary">Lista de Usuarios</h6>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                        <thead>
+                                            <tr>
+                                                <th>Imagen</th>
+                                                <th>Nombre Usuario</th>
+                                                <th>Acciones</th>
 
-      <!-- Main Content -->
-      <div id="content">
-        <?php 
+                                            </tr>
+                                        </thead>
+                                        <tfoot>
+                                            <tr>
+                                                <th>Imagen</th>
+                                                <th>Nombre Usuario</th>
+                                                <th>Acciones</th>
 
-        include_once '../componets/navBar.php';
-        ?>
-        <div class="container">
-         <h1>USUARIOS</h1>
-         <div class="card shadow mb-4">
-          <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Lista de Usuarios</h6>
-          </div>
-          <div class="card-body">
-            <div class="table-responsive">
-              <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                <thead>
-                  <tr>
-                    <th>Imagen</th>
-                    <th>Nombre Usuario</th>
-                    <th>Acciones</th>
-                    
-                  </tr>
-                </thead>
-                <tfoot>
-                 <tr>
-                  <th>Imagen</th>
-                  <th>Nombre Usuario</th>
-                  <th>Acciones</th>
+                                            </tr>
+                                        </tfoot>
 
-                </tr>
-              </tfoot>
+                                        <tbody> 
+                                            <?php
+                                            include_once "usuarioModelo.php";
+                                            $usuarioModelo = new usuarioModelo();
+                                            $result = $usuarioModelo->mostrarTodos();
+                                            $contador = 0;
+                                            while ($fila = mysqli_fetch_array($result)) {
 
-              <tbody> 
-               <?php 
-               $contador =1;
-               while ($contador <= 25) {
-                $contador++;
+                                                if ($fila != NULL) {
+                                                    $contador++;
 
-
-                echo '<tr>
-                <td><img class="img-profile rounded-circle" src="../img/prueba.jpg" width="60" height="60"></td>
-                <td>USUARIO1</td>
+                                                    echo '<tr>
+                                                        <td>' . $contador . '</td>
+                                            <td>' . $fila['user'] . '</td>                
                 
                 <td>
                 <a href="#" class="btn btn-danger btn-circle btn-sm" data-toggle="tooltip" data-placement="bottom" title="Eliminar">
@@ -87,75 +92,76 @@
               
                 </td>
                 </tr>';
-              } ?>
+                                                }
+                                            }
+                                            ?>
 
-            </tbody>
+                                        </tbody>
 
-          </table>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- End of Main Content -->
+
+
+
+                <!-- Footer -->
+                <?php
+                include_once '../componets/footer.php';
+                ?>
+                <!-- End of Footer -->
+
+            </div>
+            <!-- End of Content Wrapper -->
+
         </div>
-      </div>
-    </div>
-  </div>
+        <!-- End of Page Wrapper -->
 
-</div>
-<!-- End of Main Content -->
-
-
-
-<!-- Footer -->
-<?php 
-
-include_once '../componets/footer.php';
-?>
-<!-- End of Footer -->
-
-</div>
-<!-- End of Content Wrapper -->
-
-</div>
-<!-- End of Page Wrapper -->
-
-<!-- Scroll to Top Button-->
-<a class="scroll-to-top rounded" href="#page-top">
-  <i class="fas fa-angle-up"></i>
-</a>
+        <!-- Scroll to Top Button-->
+        <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fas fa-angle-up"></i>
+        </a>
 
 
 
 
-<!-- Bootstrap core JavaScript-->
-<script src="../vendor/jquery/jquery.min.js"></script>
-<script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- Bootstrap core JavaScript-->
+        <script src="../vendor/jquery/jquery.min.js"></script>
+        <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 
 
 
-<!-- Core plugin JavaScript-->
-<script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+        <!-- Core plugin JavaScript-->
+        <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
 
-<!-- Custom scripts for all pages-->
-<script src="../js/sb-admin-2.min.js"></script>
+        <!-- Custom scripts for all pages-->
+        <script src="../js/sb-admin-2.min.js"></script>
 
-<!-- Page level plugins -->
-<script src="../vendor/datatables/jquery.dataTables.min.js"></script>
-<script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+        <!-- Page level plugins -->
+        <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
+        <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
-<!-- Page level custom scripts -->
-<script src="../js/demo/datatables-demo.js"></script>
-<script type="text/javascript">
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-});
+        <!-- Page level custom scripts -->
+        <script src="../js/demo/datatables-demo.js"></script>
+        <script type="text/javascript">
+            $(function () {
+                $('[data-toggle="tooltip"]').tooltip()
+            });
 
-</script> 
-<script type="text/javascript">
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-});
-</script> 
+        </script> 
+        <script type="text/javascript">
+            $('#myModal').on('shown.bs.modal', function () {
+                $('#myInput').trigger('focus')
+            });
+        </script> 
 
 
 
-</body>
+    </body>
 
 </html>
