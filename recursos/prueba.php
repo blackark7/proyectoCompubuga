37c0416d14089efe636cdb8e88d3ae9f01d3485a
diego <diegoferose@hotmail.com>
2019-10-28 20:00:36 -0500

    se crear archivo de prueba

--- recursos/prueba.php
+++ recursos/prueba.php
@@ -26,3 +26,36 @@
 
 
 //    enctype="multipart/form-data"
+
+
+//    <!--modal eliminar-->
+//<div class="modal fade" id="eliminarModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+//    <div class="modal-dialog" role="document">
+//        <div class="modal-content">
+//            <div class="modal-header">
+//                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+//                </button>
+//                <h4 class="modal-title" id="myModalLabel">Eliminar</h4>
+//            </div>
+//            <form method="post" action="../instalaciones/eliminarInstalacionControlador.php" enctype="multipart/form-data">
+//                <input type="hidden" name="codigo" id="codigoNoticiaEliminar1">
+//                <div class="modal-body">
+//                    <h3>¿Esta seguro que desea eliminar la instalacion?</h3>
+//                </div>
+//                <div class="modal-footer">
+//                    <button type="button" class="btn btn-default" data-dismiss="modal">NO</button>
+//                    <button type="submit" class="btn btn-primary">SI</button>
+//                </div>
+//            </form>
+//        </div>
+//    </div>
+//</div>
+
+
+//    <a onclick='showCreateThanksYouForm(" . $fila['id'] . ")'><span class='glyphicon glyphicon-picture' </span></a>
+
+//    function showCreateThanksYouForm(v) {
+//        var valor = v;
+//        document.getElementById('codigoNoticia').value = v;
+//        $('#modal-create-thanks-you').modal('show')
+//    }
