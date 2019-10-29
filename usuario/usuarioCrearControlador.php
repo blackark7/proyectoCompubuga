6e3c074ce209a1413ccc0505fe8844e8bd055108
EQUIPO3 <EQUIPO3@EQUIPO3-PC>
2019-10-28 19:46:10 -0500

    se agrega subir foto a usuario

--- usuario/usuarioCrearControlador.php
+++ usuario/usuarioCrearControlador.php
@@ -1,8 +1,24 @@
 <?php
-    $tipo = filter_input(INPUT_POST,'tipo');
-    $usuario = filter_input(INPUT_POST,'nombreUsuario');
-    $password = filter_input(INPUT_POST,'password1');
+
+$tipo = filter_input(INPUT_POST, 'tipo');
+$usuario = filter_input(INPUT_POST, 'nombreUsuario');
+$password = filter_input(INPUT_POST, 'password1');
+
+$foto = $_FILES["foto"]["name"];
+$archivo = $_FILES["foto"]["tmp_name"];
+$destino = "../img/" . $foto;
+$move = move_uploaded_file($archivo, $destino);
+
+if (!$move) {
+    $codigo = $_FILES['foto']['error'];
+    echo 'codigo error: ' . $codigo;
+    echo 'la imagen  no pudo ser movida';
+} else {
     include_once "usuarioModelo.php";
-    $usuarioModelo  = new  usuarioModelo();
-    $usuarioModelo ->insertar($tipo,$usuario,$password);
-    header("Location: ../usuario/usuarioAdministrarVista.php");
+    $usuarioModelo = new usuarioModelo();
+    $usuarioModelo->insertar($tipo, $usuario, $password, $destino);
+}
+
+
+
+header("Location: ../usuario/usuarioAdministrarVista.php");
