4ff14624df645ad42e2ea4bdaa189798d8a7bafd
LOPEZ <LOPEZ@DESKTOP-KPU8R5E>
2019-11-05 13:16:22 -0500

    se modifica de todo un poquito

---
+++ materias/materiasEliminarControlador.php
@@ -0,0 +1,7 @@
+<?php
+$id = filter_input(INPUT_POST,'id');
+include_once 'materiasModelo.php';
+$materiasModelo = new materiasModelo();
+$materiasModelo->eliminar($id);
+header("Location: ../usuario/materiasAdministrarVista.php");
+
