182ed3406f55aef80a0a200c57d33b5ae8a5a928
LOPEZ <LOPEZ@DESKTOP-KPU8R5E>
2019-11-04 19:06:44 -0500

    se modifica de todo un poquito

--- docente/docenteCrearControlador.php
+++ docente/docenteCrearControlador.php
@@ -1,7 +1,6 @@
 <?php 
 
-include_once 'docenteModelo.php';
-$docenteModelo = new docenteModelo();
+
 
 $nombre = filter_input(INPUT_POST, 'nombre');
 $carrera = filter_input(INPUT_POST, 'carrera');
@@ -10,7 +9,22 @@ $documento = filter_input(INPUT_POST, 'documento');
 $fecha = filter_input(INPUT_POST, 'fecha');
 $direccion = filter_input(INPUT_POST, 'direccion');
 
+$foto = $_FILES["foto"]["name"];
+$archivo = $_FILES["foto"]["tmp_name"];
+$destino = "../img/" . $foto;
+$move = move_uploaded_file($archivo, $destino);
+
+if (!$move) {
+    $codigo = $_FILES['foto']['error'];
+    echo 'codigo error: ' . $codigo;
+    echo 'la imagen  no pudo ser movida';
+} else {
+include_once "docenteModelo.php";
+$docenteModelo = new docenteModelo();
 $docenteModelo->insertar($nombre,$carrera,$telefono,$documento,$fecha,$direccion);
+
+}
+
     header("Location: docenteAdministrarVista.php"); 
 
  ?>
