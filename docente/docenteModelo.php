dec6a6e6482f0f194a3f5a1070452fa520cd8f0e
LOPEZ <LOPEZ@DESKTOP-KPU8R5E>
2019-11-07 16:41:27 -0500

    se modifica de todo un poquito

--- docente/docenteModelo.php
+++ docente/docenteModelo.php
@@ -1,28 +1,36 @@
 
 <?php
+
 include_once '../conexion.php';
-    class docenteModelo
-    {
-      function insertar($nombre,$carrera, $telefono, $documento, $fecha, $direccion,$foto){
-          global $link;
-          $consulta = "INSERT INTO `docentes` (`id`, `nombre`, `carrera`, `telefono`, `documento`, `fecha`, `direccion`, `foto`) VALUES (NULL, '$nombre', '$carrera', '$telefono', '$documento', '$fecha', '$direccion','$foto');";
-          $query = mysqli_query($link, $consulta);
-      }
-      
-       function eliminar($id) {
-       
+
+class docenteModelo {
+
+    function insertar($nombre, $carrera, $telefono, $documento, $fecha, $direccion, $foto) {
+        global $link;
+        $consulta = "INSERT INTO `docentes` (`id`, `nombre`, `carrera`, `telefono`, `documento`, `fecha`, `direccion`, `foto`) VALUES (NULL, '$nombre', '$carrera', '$telefono', '$documento', '$fecha', '$direccion','$foto');";
+        $query = mysqli_query($link, $consulta);
+    }
+
+    function eliminar($id) {
+
         global $link;
         $consulta = "DELETE FROM `docentes` WHERE `docentes`.`id` = $id";
         $query = mysqli_query($link, $consulta);
     }
-      
-      
-        function mostrarTodos(){
-            global $link;
-            $consulta = "SELECT * FROM `docentes`";
-            $query = mysqli_query($link, $consulta);
-            return $query;
+
+    function modificar($nombre, $carrera, $telefono, $documento, $fecha, $direccion, $foto) {
+        global $link;
+        $consulta = "UPDATE `docentes` SET `nombre`, `carrera`, `telefono`, `documento`, `fecha`, `direccion` WHERE `docentes`.`id`";
+        $query = mysqli_query($link, $consulta);
         }
+
+    function mostrarTodos() {
+        global $link;
+        $consulta = "SELECT * FROM `docentes`";
+        $query = mysqli_query($link, $consulta);
+        return $query;
     }
+
+}
 
 ?>
