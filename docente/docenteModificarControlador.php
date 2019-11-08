d7c2f38b826332934f6ae06c6d81c0a73570f7a1
EQUIPO3 <EQUIPO3@EQUIPO3-PC>
2019-11-07 20:27:14 -0500

    se modifica de todo un poquito

--- docente/docenteModificarControlador.php
+++ docente/docenteModificarControlador.php
@@ -1,1 +1,17 @@
+<?php
+$id = filter_input(INPUT_POST, 'id');
+$nombre = filter_input(INPUT_POST, 'nombre');
+$carrera = filter_input(INPUT_POST, 'carrera');
+$telefono = filter_input(INPUT_POST, 'telefono');
+$documento = filter_input(INPUT_POST, 'documento');
+$fecha = filter_input(INPUT_POST, 'fecha');
+$direccion = filter_input(INPUT_POST, 'direccion');
 
+include_once "docenteModelo.php";
+$docenteModelo = new docenteModelo();
+$docenteModelo->modificar($nombre, $carrera, $telefono, $documento, $fecha, $direccion,$id);
+
+
+
+header("Location: docenteAdministrarVista.php");
+?>
