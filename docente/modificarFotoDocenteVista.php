a9c63cd13aed8f93706b71406f80e596f3e8a0ac
EQUIPO3 <EQUIPO3@EQUIPO3-PC>
2019-11-07 20:25:42 -0500

    se modifica de todo un poquito

---
+++ docente/modificarFotoDocenteVista.php
@@ -0,0 +1,23 @@
+<?php 
+$id = filter_input(INPUT_GET,'id');
+?>
+<html>
+    <head>
+        <title>title</title>
+    </head>
+    <body>
+        <form action="modificarFotoDocenteControlador.php" method="POST" enctype="multipart/form-data">
+            <input type="hidden" value="<?php echo $id; ?>" name="id">
+            <div class="form-group">
+                <label for="">Foto</label>
+                <input type="file" name="foto" id="" required>
+                <p class="help-block">Por favor suba una foto de perfil</p>
+            </div>
+            <hr>
+            <input type="submit" value="guardar">          
+        </form>
+
+
+    </body>
+</html>
+
