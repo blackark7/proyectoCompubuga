cd27f9b64e213ced6b0510d1d9a5be2220eee7c3
LOPEZ <LOPEZ@DESKTOP-KPU8R5E>
2019-11-14 09:55:49 -0500

    se modifica de todo un poquito

--- usuario/usuarioModificarControlador.php
+++ usuario/usuarioModificarControlador.php
@@ -3,12 +3,12 @@
 $id = filter_input(INPUT_POST, 'id');
 $tipo = filter_input(INPUT_POST, 'tipo');
 $usuario = filter_input(INPUT_POST, 'nombreUsuario');
-$password = filter_input(INPUT_POST, 'password1');
+
 
 
     include_once "usuarioModelo.php";
     $usuarioModelo = new usuarioModelo();
-    $usuarioModelo->insertar($tipo, $usuario, $password, $id);
+    $usuarioModelo->insertar($tipo, $usuario, $id);
 
 
 
