9a29458811a8c780fa7877a5afe5e20b321d0526
LOPEZ <LOPEZ@DESKTOP-KPU8R5E>
2019-12-27 17:21:01 -0500

    se modifica de todo un poquito

--- materias/materiasCrearControlador.php
+++ materias/materiasCrearControlador.php
@@ -1,13 +1,13 @@
 <?php 
 include_once 'materiasModelo.php';
-$materiasModelo = new materiasModelo();
+
 
 $nombre = filter_input(INPUT_POST, 'nombre');
 $carrera = filter_input(INPUT_POST, 'carrera');
 
 
 
-
+$materiasModelo = new materiasModelo();
 $materiasModelo->insertar($nombre,$carrera);
     header("Location: materiasAdministrarVista.php");
 
